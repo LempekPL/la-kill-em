0267bf3c7f5a75b498a707d7866edbe3e7d92633
LempekPL <superlempek2004@gmail.com>
2022-08-20 00:36:35 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod entity;
+mod camera;
+mod menus;
+mod loading;
+
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_kira_audio::AudioPlugin;
+
+fn main() {
+    let mut app = App::new();
+    app.insert_resource(WindowDescriptor {
+        title: "gairun".to_string(),
+        resizable: false,
+        width: 1280.0,
+        height: 720.0,
+        ..default()
+    });
+    app.add_plugins(DefaultPlugins);
+    app.add_plugin(AudioPlugin);
+    app.add_plugin(WorldInspectorPlugin::new());
+
+    app.run();
+}
